01b64a7b284e842a54e5d1225d448be429482372
amayc <churi.amay@gmail.com>
2020-07-03 02:18:56 +0200

    detect face

--- src/checkface.py
+++ src/checkface.py
@@ -10,6 +10,26 @@ smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_sm
 while(True):
     #capture camera frame
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+    #print region of interest
+    for (x, y, w, h) in faces:
+        print(x,y,w,h)
+        #y for height and x for width
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        img_item = "my-image.png"
+        #save image
+        cv2.imwrite(img_item,roi_gray)
+
+        #rectangle color
+        color = (255,0,0) #BGR
+        stroke = 2
+        width = x + w
+        height = y + h
+        cv2.rectangle(frame, (x, y), (width, height), color, stroke)
+
 
     cv2.imshow('frame',frame)
 
