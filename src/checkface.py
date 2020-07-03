f08fbc77df04a820d527cb1f78f81935be079d45
amayc <churi.amay@gmail.com>
2020-07-03 03:25:11 +0200

    face recognizer with basic training

--- src/checkface.py
+++ src/checkface.py
@@ -1,11 +1,22 @@
 import numpy as np
 import cv2
+import pickle
 
-cap = cv2.VideoCapture(0)
+
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_frontalface_alt2.xml')
 eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_eye.xml')
 smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_smile.xml')
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("recognizers/face-trainner.yml")
+
+labels = {"person_name": 1}
+with open("pickles/face-labels.pickle",'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
 
 while(True):
     #capture camera frame
@@ -15,10 +26,17 @@ while(True):
 
     #print region of interest
     for (x, y, w, h) in faces:
-        print(x,y,w,h)
+        # print(x,y,w,h)
         #y for height and x for width
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
+        id_, conf = recognizer.predict(roi_gray)
+        if conf>=4 and conf <= 85:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
         img_item = "my-image.png"
         #save image
         cv2.imwrite(img_item,roi_gray)
