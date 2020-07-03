7e2652b21a1633bab4c788cd23dd53c6c2ecfad6
amayc <churi.amay@gmail.com>
2020-07-03 02:05:41 +0200

    import cascades

--- src/checkface.py
+++ src/checkface.py
@@ -3,6 +3,10 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascades/haarcascade_smile.xml')
+
 while(True):
     #capture camera frame
     ret, frame = cap.read()
